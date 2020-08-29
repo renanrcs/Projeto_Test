a0e651a4146319ebbfbb7dcbf0c5590f08c8dbf6
renanrcs <renan.cavalcante@yahoo.com>
2020-08-29 09:29:21 -0300

    Adicionando folder src

---
+++ src/aula/Casa.java
@@ -0,0 +1,34 @@
+package aula;
+
+public class Casa {
+	
+	String name = "Seja Bem Vindo!!!";
+	int qtdQuartos;
+	int area = 1;
+	String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQtdQuartos() {
+		return qtdQuartos;
+	}
+	public void setQtdQuartos(int qtdQuartos) {
+		this.qtdQuartos = qtdQuartos;
+	}
+	public int getArea() {
+		return area;
+	}
+	public void setArea(int area) {
+		this.area = area;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
